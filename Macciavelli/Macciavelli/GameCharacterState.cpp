bec5bdd76b12c65d5838914b4f5c807b83803afa
Sven Brettschneider <brettschneider.sven@gmail.com>
2017-01-25 02:08:51 +0100

    Character state opzet.

--- Macciavelli/Macciavelli/GameCharacterState.cpp
+++ Macciavelli/Macciavelli/GameCharacterState.cpp
@@ -1,7 +1,8 @@
 #include "GameCharacterState.h"
 #include "GameContext.h"
+#include "Color.h"
 
-GameCharacterState::GameCharacterState(GameContext & _context, IServer & _server) : GameState{_context, _server}
+GameCharacterState::GameCharacterState(GameContext & _context, IServer & _server, CharacterType type) : GameState{_context, _server}, _type{type}
 {
 }
 
@@ -12,9 +13,18 @@ void GameCharacterState::EnterState()
 	std::vector<std::string> base_options;
 	base_options.push_back("Show money and spawned buildings of a player?");
 	base_options.push_back("Building cards you hold in you hand?");
-	//look at the table
-	
 
+	//ask the user to view some items ^^^^^^
+
+	//Add bonus money
+	current_player->MutateMoney(CalculateBonusIncome());
+
+	//Execute special character action
+	DoCharacterAction();
+
+	//ask user to get an extra card or 2 gold
+
+	//does user want to built something?
 }
 
 void GameCharacterState::LeaveState()
@@ -23,5 +33,18 @@ void GameCharacterState::LeaveState()
 
 void GameCharacterState::ShowCardsInHand()
 {
-	//for(const auto& building : _context.GetCurrentPlayer()->)
+	std::string player_name{ _context.GetCurrentPlayer()->GetName() };
+	_server.SendMessage(player_name, "You buildings are:");
+
+	for (const auto& building : _context.GetCurrentPlayer()->LookAtBuildingsInHand()) {
+		//color
+		std::string color_string{ ColorToString(building.GetColor()) };
+		std::string output{ "Color: " + color_string + "\r\n" };
+		_server.SendMessage(player_name, output);
+
+		//name / description
+		_server.SendMessage(player_name, building.GetName() + " - " + building.GetDescription());
+		_server.SendMessage(player_name, "You can buy this building for " + std::to_string(building.GetCost()) + " gold.");
+		_server.SendMessage(player_name, "------------------------------");
+	}
 }
